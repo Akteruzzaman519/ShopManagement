2cb8b18c53172edab6e0cfddc70a5a14ef55f5c6
Akteruzzaman519 <mdakteruzzaman519@gmail.com>
2021-08-30 22:27:07 +0600

    database hosting and routing modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
+import { LaptopComponent } from './laptop/laptop.component';
+import { MobileComponent } from './mobile/mobile.component';
+import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: '', component:HomeComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'product', children:[
+    {path:'', component: ProductComponent },
+    { path: 'mobile', component: MobileComponent },
+    { path: 'laptop', component: LaptopComponent }
+  ]}
+ 
+ 
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductComponent } from './product/product.component';
+import { LaptopComponent } from './laptop/laptop.component';
+import { MobileComponent } from './mobile/mobile.component';
+
+
 
 @NgModule({
   declarations: [
@@ -15,7 +20,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     HeaderComponent,
     FooterComponent,
     HomeComponent,
-    DashboardComponent
+    DashboardComponent,
+    ProductComponent,
+    LaptopComponent,
+    MobileComponent
   ],
   imports: [
     BrowserModule,
